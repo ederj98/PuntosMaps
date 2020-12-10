a2ce323347e75ebe6b2408e46ad2cad8ba6ea6c4
Eder Férnandez Baldovino - Ceiba Software <eder.fernandez@ceiba.com.co>
2020-12-10 14:38:27 -0500

    first commit

---
+++ PuntosMaps/src/main/java/com/ceiba/PuntosMapsApplication.java
@@ -0,0 +1,13 @@
+package com.ceiba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuntosMapsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuntosMapsApplication.class, args);
+	}
+
+}
---
+++ PuntosMaps/src/test/java/com/ceiba/PuntosMapsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceiba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuntosMapsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
